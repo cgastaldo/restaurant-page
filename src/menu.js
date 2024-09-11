8453838a463381295b4a261ce9c59f86d4a43efe
Chris Gastaldo <chris.gastaldo@gmail.com>
2024-09-11 19:38:23 -0400

    Added water and food entries and started formatting the layout. Most likely done as the additional CSS does not improve JS skills which is the point of the project.

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,9 @@
-import cooking from './Images/water.jpg';
+import water from './Images/water.jpg';
+import penne from './Images/penne.jpg';
+import wrap from './Images/wrap.jpg';
+import steak from './Images/steak.jpg';
 
-const homeContent = document.querySelector("#content");
+const menuContent = document.querySelector("#content");
 
 export function createMenuPage(){
     const menuHeader = document.createElement('div');
@@ -9,26 +12,47 @@ export function createMenuPage(){
     menuTitle.textContent = "Menu";
     menuHeader.append(menuTitle);
 
-    homeContent.append(menuHeader);
-
+    menuContent.append(menuHeader);
+    createBeverageList();
+    createFoodList();
 }
 
+
+
+
 function createBeverageList(){
-    bev1Title = "Water";
-    bev1Descr = "Quench your thirst with the original";
-    bev1Price = 15;
-    bev1Image = water;
+    let bev1Title = "Water";
+    let bev1Descr = "Quench your thirst with the original";
+    let bev1Price = 15;
+    let bev1Image = water;
+    createEntry(bev1Title, bev1Descr, bev1Price, bev1Image);
 
-    a = createEntry(bev1Title, bev1Descr, bev1Price, bev1Image);
-    homeContent.append(a);
-    //eateEntry(beverage2);
-
-
-
+    let bev2Title = "Not water";
+    let bev2Descr = "The beverage title might be misleading";
+    let bev2Price = 25;
+    let bev2Image = water;
+    createEntry(bev2Title, bev2Descr, bev2Price, bev2Image);
 }
 
 function createFoodList(){
+    let food1Title = "Penne";
+    let food1Descr = "No salt was used during during the cooking " +
+        "process ensuring you'll enjoy every last bit";
+    let food1Price = 28;
+    let food1Image = penne;
+    createEntry(food1Title, food1Descr, food1Price, food1Image);
 
+    let food2Title = "Meat Wrap";
+    let food2Descr = "Take the meat, take vegetables, place in wrap";
+    let food2Price = 87;
+    let food2Image = wrap;
+    createEntry(food2Title, food2Descr, food2Price, food2Image);
+
+    let food3Title = "Flame-grilled Steak with Vegetables";
+    let food3Descr = "Product may vary";
+    let food3Price = 95;
+    let food3Image = steak;
+    createEntry(food3Title, food3Descr, food3Price, food3Image);
 }
 
 function createEntry(title, description, price, image){
@@ -47,4 +71,5 @@ function createEntry(title, description, price, image){
     entryImage.className = 'entryImage';
     entryImage.src = image;
     entry.appendChild(entryImage);
+    menuContent.append(entry);
 }
